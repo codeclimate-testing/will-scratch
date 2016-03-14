4b07eabbbd6f2ee4775f28ec9dfb4e0e23cef925
Will Fleming <will@jwock.org>
2016-03-14 11:11:30 -0400

    make cli better

---
+++ ruby/cli.rb
@@ -0,0 +1,16 @@
+require 'log4r'
+require 'optparse'
+
+module Vagrant
+  # Manages the command line interface to Vagrant.
+  class CLI < Vagrant.plugin("2", :command)
+    def initialize(argv, env)
+      super
+
+      @logger = Log4r::Logger.new("vagrant::cli")
+      @main_args, @sub_command, @sub_args = split_main_and_subcommand(argv)
+
+      @logger.info("CLI: #{@main_args.inspect} #{@sub_command.inspect} #{@sub_args.inspect}")
+    end
+  end
+end
